0a9950266785c0ee4503d3ec62f6f053f1763dac
aria1991 <47612848+aria1991@users.noreply.github.com>
2022-12-23 12:21:59 +0100

    Update Utest.py

--- Utest.py
+++ Utest.py
@@ -1,5 +1,6 @@
 import unittest
-
+import os
+import json
 from mock import Mock, patch
 
 # Import the code that you want to test
@@ -17,22 +18,43 @@ class TestAPI(unittest.TestCase):
         self.mock_request_response.status_code = 200
         self.mock_request_response.text = "OK"
 
+        # Create a mock JSON file with the APIs
+        api_list = [
+            {
+                "name": "API 1",
+                "request": {
+                    "method": "GET",
+                    "url": "https://api.example.com/endpoint1"
+                }
+            },
+            {
+                "name": "API 2",
+                "request": {
+                    "method": "POST",
+                    "url": "https://api.example.com/endpoint2",
+                    "body": {
+                        "mode": "raw",
+                        "raw": "{\"key\":\"value\"}"
+                    }
+                }
+            }
+        ]
+        with open("apis.json", "w") as f:
+            json.dump(api_list, f)
+
+    def tearDown(self):
+        # Delete the mock JSON file with the APIs
+        if os.path.exists("apis.json"):
+            os.remove("apis.json")
+
+        # Delete the mock JSON file with the results
+        if os.path.exists("results.json"):
+            os.remove("results.json")
+
     @patch("api_testing.PostmanAPI")
     @patch("api_testing.requests")
     def test_api_testing(self, mock_requests, mock_postman_api):
         # Set up the mock responses for the PostmanAPI and requests libraries
         mock_postman_api.return_value.collections.create.return_value = self.mock_collection_response
         mock_postman_api.return_value.requests.create.return_value = None
-        mock_requests.request.return_value = self.mock_request_response
-
-        # Run the code that you want to test
-        api_testing.main()
-
-        # Assert that the expected calls were made to the PostmanAPI and requests libraries
-        mock_postman_api.assert_called_once_with("YOUR_API_KEY")
-        mock_postman_api.return_value.collections.create.assert_called_once_with(name="My Collection")
-        mock_postman_api.return_value.requests.create.assert_called_twice()
-        mock_requests.request.assert_called_twice()
-
-if __name__ == "__main__":
-    unittest.main()
+        mock_requests.request.return_value = self.mock
